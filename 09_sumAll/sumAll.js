cc9247bec40c544d0b87ce4d74588ed1d5639f45
danilunac <182788341+danilunac@users.noreply.github.com>
2025-11-28 17:08:06 +0100

    feat: complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
 
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    }
+
+    // Array destructuring syntax
+    // if (start > end) [start, end] = [end, start];
+
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let count = 0;
+    for (let i = start; i <= end; i++) {
+        count += i
+    }
+
+    return count;
 };
+
+let test = sumAll(1, 10);
+
+console.log(test);
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
