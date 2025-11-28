8424b80c613ceaa6c2f50735965e6ee9dc1b9a66
danilunac <182788341+danilunac@users.noreply.github.com>
2025-11-28 17:09:03 +0100

    feat: complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemToRemove) {
+    return arr.filter(item => !itemToRemove.includes(item));
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+let test = removeFromArray([1, 2, 3, 4], 2, 3, 'nope');
+console.log(test);
