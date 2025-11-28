52fc6fd43c5d3289ee149e32a1d7083483e1d1ea
danilunac <182788341+danilunac@users.noreply.github.com>
2025-11-28 17:09:46 +0100

    feat: complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    // return word.split('').reverse().join('');
+    
+    let reverseWord = [];
 
+    for (let i = 0; i < word.length; i++) {
+        reverseWord.push(word[i]);
+    }
+
+    return reverseWord.reverse().join('');
 };
+
+console.log(reverseString('hola'));
 
 // Do not edit below this line
 module.exports = reverseString;
