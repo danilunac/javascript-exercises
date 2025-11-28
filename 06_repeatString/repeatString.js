171babb81e321c7ea29351f7ea85a48c01d991ba
danilunac <182788341+danilunac@users.noreply.github.com>
2025-11-28 17:10:20 +0100

    feat: complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // return string.repeat(num);
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    let newString = [];
+    for (let i = 0; i < num; i++) {
+        newString.push(string);
+    }
+
+    return newString.join('');
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
