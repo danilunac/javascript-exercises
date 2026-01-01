c1282331f6ada86e0cb2b9a75d82bc2f0af98f6f
danilunac <182788341+danilunac@users.noreply.github.com>
2026-01-01 12:26:48 +0100

    feat: Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, value) => acc + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, value) => acc * value, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let counter = 1;
+	for (let i = 0; i < b; i++) {
+    counter *= a;
+    console.log(counter);
+  } 
+  
+  return counter;
+   /*
+  return a ** b;
+  */
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  // let counter = 1;
+	// for (let i = 1; i < num; i++) {
+  //   counter *= (i + 1)
+  // }
+  
+  // return counter;
+
+  /* 
+  // TOP solution
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+  */
+  
+  // Recursive function
+  if (num === 0) return 1;
+  return num * factorial(num - 1);  
 };
 
 // Do not edit below this line
