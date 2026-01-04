0b06075daf4672d6dea2ee48b8f4273b3b7fd061
danilunac <182788341+danilunac@users.noreply.github.com>
2026-01-04 17:48:59 +0100

    feat: Complete 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    // Inmutable version (creates a new array at each step)
+    // return arr.reduce((acc, current) => ([
+    //   ...acc,
+    //   current.title,
+    // ]), []);
 
+    // Mutable version (modifies the same array using indexes)
+    return arr.reduce((acc, current, index) => {
+    acc[index] = current.title;
+    return acc;
+    }, [])
+
+    // Solution with map()
+    // return arr.map((book) => book.title);
 };
 
 // Do not edit below this line
